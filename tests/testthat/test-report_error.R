62ddbed470820891236d15c7117f4591905d28df
FvD <frans@ixpantia.com>
2022-10-24 18:09:49 -0600

    Closes #44

--- tests/testthat/test-report_error.R
+++ tests/testthat/test-report_error.R
@@ -5,6 +5,6 @@ test_that("Error report is being sent to the correct API", {
   res <- report_error(message)
   content <- httr::content(res)
 
-  expect_equal(content$error$message, "Invalid JSON payload received. Unknown name \"\": Root element must be a message.")
+  expect_true(content$error$code %in% (c("400", "403")))
 
 })
